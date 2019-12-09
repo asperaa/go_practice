44a12dc6e6e7c8093e6d6dee0521636c9da405f0
asperaa <adityaankur44@gmail.com>
2019-12-09 17:44:29 +0530

    feat: arrays in go

---
+++ arrays.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	var color [2]string
+	color[0] = "Black"
+	color[1] = "Blue"
+	fmt.Println(color)
+
+	odd := [6]int{2, 3, 5, 7, 11, 13}
+	fmt.Println(odd)
+}
